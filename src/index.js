1fa0a0fab6705a4ac6689b271ac7bf044e897e47
Jerry Hamlet <jerry@logichub.com>
2018-06-02 10:01:24 -0700

    Add clean task and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import * as predicates from './predicates';
-import * as projections from './projections';
-
 export * from './predicates';
 export * from './projections';
-
-export default {
-  ...predicates,
-  ...projections
-};
 
